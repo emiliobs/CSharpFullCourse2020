c15c62d059f9acf7b8db55a7a4f72ea89e5950be
Emilio <barrera_emilio@yahoo.es>
2020-05-25 11:49:22 +0100

    Start Wpf  BBDD with Sql Server 209

--- WpfRadioButton/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ WpfRadioButton/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace WpfRadioButton {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfRadioButton;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfRadioButton;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
