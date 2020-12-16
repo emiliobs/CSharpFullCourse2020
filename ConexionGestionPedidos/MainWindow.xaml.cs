c49f13dffbc9769e2a5be53299ea5bc8db9656b0
Emilio Barrea <barrera_emilio@yahoo.es>
2020-12-16 01:51:57 +0000

    Mostrar Datos en wpf

---
+++ ConexionGestionPedidos/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace ConexionGestionPedidos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly SqlConnection miConexionSql;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string miConexion = ConfigurationManager.ConnectionStrings["ConexionGestionPedidos.Properties.Settings.GestionPedidosConnectionString"].ConnectionString;
+            miConexionSql = new SqlConnection(miConexion);
+
+        }
+
+        private void MostrarClientes()
+        {
+            var consulta = "Select * From Cliente";
+        }
+    }
+}
--- WpfComboBoxAndCheckBox/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ WpfComboBoxAndCheckBox/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -103,13 +103,13 @@ namespace WpfComboBoxAndCheckBox {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfComboBoxAndCheckBox;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfComboBoxAndCheckBox;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -119,7 +119,7 @@ namespace WpfComboBoxAndCheckBox {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
